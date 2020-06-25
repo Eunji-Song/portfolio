ef57ee2dcfdadbbac56f8a50d2f059ae804d241b
Eunji-Song <eunjisong99@gmail.com>
2020-06-26 01:02:50 +0900

    도형 추가

--- ver_notion/index.php
+++ ver_notion/index.php
@@ -9,25 +9,26 @@
     </head>
     <body>
         <div class="wrap">
-            <!-- 오각형 -->
+
             <section class="main">
+                <!-- 오각형 -->
+                <div class="shape-wrap">
+                    <div class="" id="pentagon">
+                    </div>
+                </div>
+
                 <div class="container">
                     <h1>안녕하세요 👋 <br>발전하기 위해 공부하는<br> 웹개발자 송은지입니다. 👩‍💻</h1>
-                    <!-- <h2>발전하기 위해 공부하고 노력하는 웹 개발자</h2> -->
-                    <p class="ex">
-                        사용자를 위해 좋은 서비스를 만들고 코딩을 합니다.<br>
-                        효율적인 업무방식을 지향합니다. 비효율은 죄송하지만 꺼져주세요 <br>
-                        저를 뽑으시려면 최소한 1억은 주셔야 합니다. 안그러면 꺼져
+                    <p class="introduce">
+                        서버까지 컨트롤이 가능한 개발자가 되는 것이 저의 목표입니다.<br>
+                        다양한 기술을 익히기 위해서 강의를 듣거나 독학을 합니다.<br>
+                        열심히 배우겠습니다. <br>
                     </p>
-
                     <hr>
-                    <!-- <p class="ex">
-                        안녕하세요. 백엔드 개발자 송은지입니다.
-                    </p> -->
-                    <div class="wrap-box">
+                    <div class="inner-container flex-wrap">
                         <div class="experience box">
                             <div class="emoji">😎</div>
-                            <h4>Experience</h4>
+                            <h2>Experience</h2>
                             <ul>
                                 <li><b>2015.03 ~ 2018.02</b> 서울디지텍 고등학교</li>
                                 <li><b>2017.08 ~</b> appknot 웹 개발자</li>
@@ -35,17 +36,16 @@
                         </div>
                         <div class="skil box">
                             <div class="emoji">🔨</div>
-                            <h4>Skil</h4>
+                            <h2>Skil</h2>
                             <ul>
                                 <li>HTML/CSS,PHP</li>
-                                <!-- <li></li> -->
+                                <li>jQuery,javaScript</li>
                                 <li>MySQL,AWS</li>
-                                <!-- <li></li> -->
                             </ul>
                         </div>
                         <div class="license box">
                             <div class="emoji">🏆</div>
-                            <h4>License</h4>
+                            <h2>License</h2>
                             <ul>
                                 <li><b>2016.06.10</b> 웹 디자인 기능사 취득</li>
                                 <li><b>2017.07.05</b>  정보처리 기능사</li>
@@ -55,37 +55,33 @@
                 </div>
             </section>
             <section class="project">
-                <div class="bg">
-                    <div class="top"></div>
-                    <div class="bottom"></div>
-                </div>
                 <div class="container">
-                    <h3>Project</h3>
-                    <div class="box">
+                    <h2>Project</h2>
+                    <div class="inner-wrap">
                         <div style="background:#ddd url(./res/image/project-weat-1.jpg) no-repeat center center ">
                             <div class="detail">
-                                <h4>Weathernews</h4>
+                                <h3>Weathernews</h3>
                                 <p>API,관리자 페이지 FE,BE 개발 </p>
                                 <button type="button" name="button">More</button>
                             </div>
                         </div>
-                        <div style="background-image:url(./res/image/project-amore-1.jpg)">
+                        <div style="background:#ddd url(./res/image/project-amore-1.jpg) no-repeat center center ">
                             <div class="detail">
-                                <h4>GTR/CTR</h4>
+                                <h3>Weathernews</h3>
                                 <p>API,관리자 페이지 FE,BE 개발 </p>
                                 <button type="button" name="button">More</button>
                             </div>
                         </div>
                         <div style="background-image:url(./res/image/project-wassup-1.jpg)">
                             <div class="detail">
-                                <h4>Wassup</h4>
+                                <h3>Wassup</h3>
                                 <p>API,관리자 페이지 FE,BE 개발 </p>
                                 <button type="button" name="button">More</button>
                             </div>
                         </div>
                         <div style="background-image:url(./res/image/project-husales-1.jpg)">
                             <div class="detail">
-                                <h4>Husales</h4>
+                                <h3>Husales</h3>
                                 <p>API,관리자 페이지 FE,BE 개발 </p>
                                 <button type="button" name="button">More</button>
                             </div>
@@ -93,68 +89,25 @@
 
                         <div style="background-image:url(./res/image/project-crushgiant-1.jpg)">
                             <div class="detail">
-                                <h4>철거인/수거인</h4>
+                                <h3>철거인/수거인</h3>
                                 <p>API,관리자 페이지 FE,BE 개발 </p>
                                 <button type="button" name="button">More</button>
                             </div>
                         </div>
                         <div style="background-image:url(./res/image/project-chunil-1.jpg)">
                             <div class="detail">
-                                <h4>Chunil ERP</h4>
+                                <h3>Chunil ERP</h3>
                                 <p>API,관리자 페이지 FE,BE 개발 </p>
                                 <button type="button" name="button">More</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <p class="contact">dddd</p>
-                <!-- <div class="container">
-                    <div class="box">
-                        <div style="background:#ddd url(./res/image/project-weat-1.jpg) no-repeat center center ">
-                            <div class="detail">
-                                <h4>Weathernews</h4>
-                                <p>API,관리자 페이지 FE,BE 개발 </p>
-                                <button type="button" name="button">More</button>
-                            </div>
-                        </div>
-                        <div style="background-image:url(./res/image/project-amore-1.jpg)">
-                            <div class="detail">
-                                <h4>GTR/CTR</h4>
-                                <p>API,관리자 페이지 FE,BE 개발 </p>
-                                <button type="button" name="button">More</button>
-                            </div>
-                        </div>
-                        <div style="background-image:url(./res/image/project-wassup-1.jpg)">
-                            <div class="detail">
-                                <h4>Wassup</h4>
-                                <p>API,관리자 페이지 FE,BE 개발 </p>
-                                <button type="button" name="button">More</button>
-                            </div>
-                        </div>
-                        <div style="background-image:url(./res/image/project-husales-1.jpg)">
-                            <div class="detail">
-                                <h4>Husales</h4>
-                                <p>API,관리자 페이지 FE,BE 개발 </p>
-                                <button type="button" name="button">More</button>
-                            </div>
-                        </div>
-
-                        <div style="background-image:url(./res/image/project-crushgiant-1.jpg)">
-                            <div class="detail">
-                                <h4>철거인/수거인</h4>
-                                <p>API,관리자 페이지 FE,BE 개발 </p>
-                                <button type="button" name="button">More</button>
-                            </div>
-                        </div>
-                        <div style="background-image:url(./res/image/project-chunil-1.jpg)">
-                            <div class="detail">
-                                <h4>Chunil ERP</h4>
-                                <p>API,관리자 페이지 FE,BE 개발 </p>
-                                <button type="button" name="button">More</button>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
+            </section>
+            <section class="contact">
+                <div class="container">
+                    eunjisong99@gmail.com
+                </div>
             </section>
         </div>
     </body>
